9d7ff8dd7652a9c97a0be6fb58d29a2e644ed01f
andreynho2006 <andreynho2006@gmail.com>
2018-07-24 13:51:55 +0300

    create the http server and listen to port 3000

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var path = require("path");
 var mime = require("mime");
 var cache = {};
 
+// error responses
 function send404(response) {
     response.writeHead(404, {'Content-type': 'text/plain'});
     response.write('Error 404: resource not found.');
     response.end();
 }
 
+//sending files
 function sendFile(response, filePath, fileContents) {
     response.writeHead(
         200, {'content-type': mime.lookup(path.basename(filePath)) } 
@@ -17,23 +19,42 @@ function sendFile(response, filePath, fileContents) {
     response.end(fileContents);
 }
 
+//serve static files from cache  memory
 function serveStatic(response, cache, absPath) {
-    if(cache[absPath]) {
-        sendFile(response, absPath, cache[absPath]);
+    if(cache[absPath]) {                                              //check if file is cached in memory
+        sendFile(response, absPath, cache[absPath]);                  //serve file from memory
     } else {
-        fs.exists(absPath, function(exists) {
+        fs.exists(absPath, function(exists) {                         //check if file exists
             if(exists) {
-                fs.readFile(absPath, function(err, data) {
+                fs.readFile(absPath, function(err, data) {            //read file from disk
                     if(err) {
                         send404(response);
                     } else {
                         cache[absPath] = data;
-                        sendFile(response, absPath, data);
+                        sendFile(response, absPath, data);            //serve file read from disk
                     }
                 });
             } else {
-                send404(response);
+                send404(response);                                    //send 404 HTTP response
             }
         });
     }
 }
+
+//create the HTTP server
+var server = http.createServer(function(request, response) {           //create HTTP server, using anonymous function to define per-request behaviour
+    var filePath = false;
+
+    if(request.url == "/") {
+        filePath = "public/index.html";                                //determine HTML file to be served by default
+    } else {
+        filePath = "public" + request.url;                             //translate URL path to reltive file path
+    }
+
+    var absPath = "./" + filePath;
+    serveStatic(response, cache, absPath);                            // serve static file
+});
+
+server.listen(3000, function() {
+    console.log("Server listening on port 3000.");
+});
