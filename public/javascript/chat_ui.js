4f868b222fb6570b2b2e383e5ce19c3168568598
andreynho2006 <andreynho2006@gmail.com>
2018-07-26 11:30:06 +0300

    add function to process the user input

--- public/javascript/chat_ui.js
+++ public/javascript/chat_ui.js
@@ -7,3 +7,21 @@ function divEscapedContentElement(message) {
 function divsystemContentElement(message) {
     return $("<div></div>").html("<i>" + message + "</i>");
 }
+
+// Function to process the raw user input
+function processUserInput(chatApp, socket) {
+    var message = $("#send-message").val();
+    var systemMessage;
+
+    if (message.charAt(0) == "/") {                                                // If user input begins with slash, treat it as command
+        systemMessage = chatApp.processCommand(message);
+        if(systemMessage) {
+            $("#messages").append(divsystemContentElement(systemMessage));
+        }
+    } else {
+        chatapp.sendMessages($("#room").text(), message);                          // Broadcast noncommand input to other users
+        $("#messages").append(divEscapedContentElement(message));
+        $("#messages").scrollTop($("#messages").prop("scrollHeight"));
+    }
+    $("#send-message").val("");
+}
